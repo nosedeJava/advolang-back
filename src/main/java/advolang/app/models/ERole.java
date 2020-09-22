7fd8d50ea8feb80ac94b02f2e3b78e860860a76b
migue1994 <migue24co@gmail.com>
2020-09-22 12:57:34 -0500

    Dabase conection and mome models

---
+++ src/main/java/advolang/app/models/ERole.java
@@ -0,0 +1,7 @@
+package advolang.app.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/advolang/app/models/User.java
@@ -0,0 +1,29 @@
+package advolang.app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @DBRef
+    private Set<Role> roles = new HashSet<>();
+}
