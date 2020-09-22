af97036a559803c8cb2d4e7ab54b308c5271d277
urobs17 <michael.preciado@mail.escuelaing.edu.co>
2020-09-21 19:29:20 -0500

    backend initialized

---
+++ src/main/java/advolang/app/AdvolangApplication.java
@@ -0,0 +1,13 @@
+package advolang.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvolangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvolangApplication.class, args);
+	}
+
+}
